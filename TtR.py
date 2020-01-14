fc1edc8b4a0f69df3fd9b0315590fd092ba65dd0
rakulb <rakulb@ttu.ee>
2020-01-14 22:59:19 +0200

    Added Ticket to Ride and some progress of Battleships board

---
+++ TtR.py
@@ -0,0 +1,158 @@
+cost_of_station = 4
+number_of_stations = 3
+
+class Player:
+	def __init__(self, name):
+		self.name = name.capitalize()
+		self.score = cost_of_station * number_of_stations
+		self.stations_left = number_of_stations
+
+	def add_points(self, score):
+		self.score += score
+
+	def __repr__(self):
+		return self.name
+
+
+def ask_input(player):
+	length = ''
+	railroad_points = {
+		0: 0,
+		1: 1,
+		2: 2,
+		3: 4,
+		4: 7,
+		6: 15,
+		8: 21,
+	}
+	while not (length.isdigit() or len(length) > 1 and length[0] == '-' and length[1:].isdigit()):
+		length = input(f"{player} is building a train.\nHow long is the train? ")
+		if length == '':
+			print("Skip")
+			return 0
+		elif length.lower() == 'end':
+			return 'end'
+		elif length.lower() == 'station':
+			if player.stations_left > 0:
+				print(f'Subtracting cost of station from {player}.')
+				player.stations_left -= 1
+				return -cost_of_station
+			else:
+				print(f'{player} has no more stations!')
+				return 0
+	if length[0] == '-':
+		print(f'Subtracting {length[1:]} points from {player}.')
+		return int(length)
+	number_of_points = railroad_points.get(int(length))
+	if number_of_points:
+		print(f'Train with a length of {length} is worth {number_of_points} points.\nAdding {player} {number_of_points} points.')
+		return number_of_points
+	else:
+		print('Invalid length!')
+		return 0
+
+
+def print_table(list_of_players, turn):
+    table_list = ["╔", "║", "╟", "║", "╚"]
+    for i, player in enumerate(list_of_players):
+        name = player.name
+        if i == turn:
+            name = name.upper()
+        cell_width = max(len(player.name), 5, len(str(player.score)))
+        table_list[0] += '═' * (cell_width + 2) + "╦"
+        table_list[1] += f" {name:>{cell_width}} ║"
+        table_list[2] += '─' * (cell_width + 2) + "╫"
+        table_list[3] += f" {player.score:>{cell_width}} ║"
+        table_list[4] += '═' * (cell_width + 2) + "╩"
+    table_list[0] = table_list[0][:-1] + "╗"
+    table_list[2] = table_list[2][:-1] + "╢"
+    table_list[4] = table_list[4][:-1] + "╝"
+    return "\n".join(table_list)
+	
+def print_end_table(list_of_players):
+	table_list = ["╔", "║", "╟" , "║", "╚"]
+	for player in list_of_players:
+		cell_width = max(len(player.name), 5, len(str(player.score)))
+		table_list[0] += '═' * (cell_width + 2) + "╦"
+		table_list[1] += f" {player.name:>{cell_width}} ║"
+		table_list[2] += '─' * (cell_width + 2) + "╫"
+		table_list[3] += f" {player.score:>{cell_width}} ║"
+		table_list[4] += '═' * (cell_width + 2) + "╩"
+	table_list[0] = table_list[0][:-1] + "╗"
+	table_list[2] = table_list[2][:-1] + "╢" 
+	table_list[4] = table_list[4][:-1] + "╝" 	
+	return "\n".join(table_list)
+
+
+def ask_number_of_players():
+	number_of_players = input("How many players are there? ")
+	while not number_of_players.isdigit() or int(number_of_players) > 5 or int(number_of_players) < 1:
+		number_of_players = input("Insert the number of players (1-5)! ")
+	return int(number_of_players)
+
+def ask_names(number_of_players):
+	players_list = list()
+	for i in range(number_of_players):
+		new_name = input(f"Player {i+1}:")
+		players_list.append(Player(new_name))
+	return players_list
+
+
+def add_ten(players):
+	while True:
+		longest = input("Who got the longest continous railroad? ")
+		for player in players:
+			if player.name.capitalize() == longest.capitalize():
+				print(f'Adding {player} 10 points for the longest continous railroad.')
+				player.score += 10
+				return True
+		print("There's no such player!")
+
+def ask_bonus(player):
+	score = ''
+	while not (score.isdigit() or len(score) > 1 and score[0] == '-' and score[1:].isdigit()):
+		score = input(f"Task-points for {player}\nHow many extra points are awarded? ")
+	return int(score)
+	
+
+def game_loop():
+	number_of_players = ask_number_of_players()
+	players = ask_names(number_of_players)
+	turn = 0
+	print(print_table(players, turn))
+	while True:
+		score = ask_input(players[turn])
+		if score == 'end':
+			break
+		players[turn].add_points(score)
+		turn = (turn + 1) % len(players)
+		print("Scores:")
+		print(print_table(players, turn))
+	return players
+	
+	
+def end(players):
+	add_ten(players)
+	for player in players:
+		additional_score_for_tasks = ask_bonus(player)
+		print(f"Adding {player} {additional_score_for_tasks} points.")
+		player.score += additional_score_for_tasks
+	print("Final scores:")
+	print(print_end_table(players))
+		
+		
+def main():
+	while True:
+		players = game_loop()
+		end(players)
+		
+		input_text = ''
+		print("Insert 'new game' to start a new game or Press Ctrl+C to exit.")
+		
+		while input_text != 'new game':
+			input_text = input().lower()
+	
+    
+	
+if __name__ == '__main__':
+    main()
---
+++ battleship.py
@@ -0,0 +1,152 @@
+EMPTY = ' '
+SHIP = '░'
+HIT = '▒'
+SUNK = '▓'
+MISS = 'X'
+ships = [1, 1, 1, 1, 2, 2, 2, 3, 3, 4]
+def generate_field():
+	return [[EMPTY for _ in range(10)] for _ in range(10)]
+	
+	
+def add_ship(field, coords1, coords2):
+	row_1 = int(coords1[1:]) - 1
+	row_2 = int(coords2[1:]) - 1
+	col_1 = ord(coords1[0]) - 65
+	col_2 = ord(coords2[0]) - 65
+	if (row_1 == row_2 or col_1 == col_2) and check_ship(field, row_1, row_2, col_1, col_2):
+		for row in range(min(row_1, row_2), max(row_1, row_2) + 1):
+			for square in range(min(col_1, col_2), max(col_1, col_2) + 1):
+				field[row][square] = SHIP
+	return field
+
+
+def check_ship(field, row_1, row_2, col_1, col_2):
+	if abs(row_1 - row_2) < 5 and abs(col_1 - col_2) < 5:
+		for row in range(max(0, min(row_1 - 1, row_2 - 1)), min(max(row_1 + 2, row_2 + 2), 10)):
+				for square in range(max(0, min(col_1 - 1, col_2 - 1)), min(max(col_1 + 2, col_2 + 2), 10)):
+					if field[row][square] != EMPTY:
+						print(row, square)
+						print(field[row][square])
+						print('EMPTY FAILS!')
+						print('CHECK FAILS!')
+						return False
+		ship_size = max(abs(row_1 - row_2) + 1, abs(col_1 - col_2) + 1)
+		if ship_size in ships:
+			ships.remove(ship_size)
+			return True
+		else:
+			print('EI SAA SEDA LAEVA PANNA!')
+	print('CHECK FAILS!')
+	return False
+
+
+def hit(field, coords):
+	row = int(coords[1:]) - 1
+	col = ord(coords[0]) - 65
+	field[row][col] = HIT
+	return field
+	
+	
+def miss(field, coords):
+	row = int(coords[1:]) - 1
+	col = ord(coords[0]) - 65
+	field[row][col] = MISS
+	return field
+
+
+def sink_ship(field, coords, cache=list()):
+	row = int(coords[1:]) - 1
+	col = ord(coords[0]) - 65
+	field[row][col] = SUNK
+	for i in range(max(0, row - 1), min(10, row + 2)):
+		for j in range(max(0, col - 1), min(10, col + 2)):
+			if (i, j) in cache:
+				pass
+			elif field[i][j] == HIT:
+				cache.append((i, j))
+				sink_ship(field, chr(j+65)+str(i+1), cache)
+				field[i][j] = SUNK
+			elif field[i][j] == EMPTY:
+				field[i][j] = MISS
+	return field
+	
+	
+def print_field(my_field, enemy_field):
+	print('  ABCDEFGHIJ ABCDEFGHIJ ')
+	for i, row in enumerate(my_field):
+		string_to_print = f"{i+1:>2}"
+		for square in row:
+			string_to_print += square
+		string_to_print += "|"
+		for square in enemy_field[i]:
+			string_to_print += square
+		string_to_print += str(i+1)
+		print(string_to_print)
+	print('  ABCDEFGHIJ ABCDEFGHIJ ')
+
+
+def get_count(what_to_count, field):
+	count = 0
+	for row in field:
+		for square in row:
+			if square == what_to_count:
+				count += 1
+	return count
+
+def check_end(my_field, enemy_field):
+	return get_count(SUNK, my_field) == 20 or get_count(SUNK, enemy_field) == 20
+
+
+def players_turn(my_field, enemy_field):
+	action = ''
+	while action != 'miss':
+		square_to_put = input('Mis ruut?').upper()
+		action = input('Mis juhtus?').lower()
+		if action == 'hit':
+			enemy_field = hit(enemy_field, square_to_put)
+		elif action == 'sunk':
+			enemy_field = sink_ship(enemy_field, square_to_put)
+		elif action == 'miss':
+			enemy_field = miss(enemy_field, square_to_put)
+		print_field(my_field, enemy_field)
+
+def get_square(field, coords):
+	row = int(coords[1:]) - 1
+	col = ord(coords[0]) - 65
+	return field[row][col]
+	
+
+def opponents_turn(my_field, enemy_field):
+	print('Vastase käik')
+	missed = False
+	while not missed:
+		square_to_put = input('Mis ruut?').upper()
+		type = get_square(my_field, square_to_put)
+		if type == HIT:
+			my_field = hit(my_field, square_to_put)
+		elif type == EMPTY:
+			my_field = miss(my_field, square_to_put)
+			missed = True
+	print_field(my_field, enemy_field)
+	
+	
+def play():
+	my_field = generate_field()
+	enemy_field = generate_field()
+	print_field(my_field, enemy_field)
+	#while get_count(SHIP, my_field) != 20:
+	#	ship_coords = input("Kuhu panna laev? Ots1-Ots2: ").upper().split('-')
+	#	if len(ship_coords) == 1:
+	#		ship_coords.append(ship_coords[0])
+	#	my_field = add_ship(my_field, ship_coords[0], ship_coords[1])
+	#	print_field(my_field, enemy_field)
+	while not check_end(my_field, enemy_field):
+		players_turn(my_field, enemy_field)
+		opponents_turn(my_field, enemy_field)
+	if get_count(SUNK, my_field) == 20:
+		print('Vastane võitis!')
+	else:
+		print('Sinu võit!')
+
+if __name__ == '__main__':
+	play()
